ff65a9c83de10adb654c058c5c5413ad5dfaa4dd
Aquarium <dmitro2006pozniak@gmail.com>
2026-03-14 13:29:57 +0000

    feat: add http handler for POST method

---
+++ internal/api/http_handler.go
@@ -0,0 +1,47 @@
+package api
+
+import (
+	"errors"
+	"net/http"
+
+	"test/internal/models"
+	"test/internal/service"
+
+	"github.com/labstack/echo/v4"
+)
+
+type HTTPHandler struct {
+	activityService service.ActivityService
+}
+
+func NewHTTPHandler(activityService service.ActivityService) *HTTPHandler {
+	return &HTTPHandler{
+		activityService: activityService,
+	}
+}
+
+func (h *HTTPHandler) HandleCreateEvent(c echo.Context) error {
+	var event models.Event
+
+	if err := c.Bind(&event); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{
+			"error": "invalid JSON format",
+		})
+	}
+
+	err := h.activityService.CreateEvent(c.Request().Context(), &event)
+
+	if err != nil {
+		if errors.Is(err, service.ErrInvalidUserID) || errors.Is(err, service.ErrInvalidAction) {
+			return c.JSON(http.StatusBadRequest, map[string]string{
+				"error": err.Error(),
+			})
+		}
+
+		return c.JSON(http.StatusInternalServerError, map[string]string{
+			"error": "internal server error while saving event",
+		})
+	}
+
+	return c.JSON(http.StatusCreated, event)
+}
