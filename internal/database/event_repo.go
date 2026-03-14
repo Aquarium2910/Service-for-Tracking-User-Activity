0ef68017da5588bdfc9b1ccdbc4cc5fefb277cbd
Aquarium <dmitro2006pozniak@gmail.com>
2026-03-14 14:08:13 +0000

    feat: add GetEvents method to repo

--- internal/database/event_repo.go
+++ internal/database/event_repo.go
@@ -10,6 +10,7 @@ import (
 
 type EventRepo interface {
 	Create(ctx context.Context, event *models.Event) error
+	GetEvents(ctx context.Context, filter *models.EventFilter) ([]models.Event, error)
 }
 
 type eventRepo struct {
@@ -32,4 +33,47 @@ func (r *eventRepo) Create(ctx context.Context, event *models.Event) error {
 	}
 
 	return nil
+}
+
+func (r *eventRepo) GetEvents(ctx context.Context, filter *models.EventFilter) ([]models.Event, error) {
+	query := `SELECT id, user_id, action, metadata, created_at FROM events WHERE user_id = $1`
+
+	args := []any{filter.UserID}
+
+	argId := 2
+
+	if !filter.StartDate.IsZero() {
+		query += fmt.Sprintf(" AND created_at >= $%d", argId)
+		args = append(args, filter.StartDate)
+		argId++
+	}
+	if !filter.EndDate.IsZero() {
+		query += fmt.Sprintf(" AND created_at <= $%d", argId)
+		args = append(args, filter.EndDate)
+		argId++
+	}
+
+	query += " ORDER BY created_at DESC"
+
+	rows, err := r.db.Query(ctx, query, args...)
+	if err != nil {
+		return nil, fmt.Errorf("EventRepo.GetEvents - failed to execute query: %w", err)
+	}
+	defer rows.Close()
+
+	var events []models.Event
+	for rows.Next() {
+		var e models.Event
+		err := rows.Scan(&e.ID, &e.UserID, &e.Action, &e.Metadata, &e.CreatedAt)
+		if err != nil {
+			return nil, fmt.Errorf("EventRepo.GetEvents - failed to scan row: %w", err)
+		}
+		events = append(events, e)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("EventRepo.GetEvents - rows iteration error: %w", err)
+	}
+
+	return events, nil
 }
